b4b417bd5b9a26828d114f747c1df2476b18f5da
gavvahar <nihargavva@gmail.com>
2022-09-19 14:16:32 -0400

    part 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import Gallery from "./components/Gallery";
 import SearchBar from "./components/SearchBar";
+import { DataContext } from "./context/DataContext";
 
 function App() {
   let [search, setSearch] = useState("");
   let [message, setMessage] = useState("Search for Music!");
-  let [data, setData] = useState([]);
+  // let [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,10 +25,12 @@ function App() {
   }, [search]);
 
   return (
-    <div>
-      <SearchBar />
+    <div className="App">
+      <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery />
+      <DataContext.Provider value={data}>
+        <Gallery />
+      </DataContext.Provider>
     </div>
   );
 }
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,15 @@
+import { useContext } from "react";
+import { DataContext } from "../context/DataContext";
 import GalleryItem from "./GalleryItem";
 
-function Gallery(props) {
-  return (
-    <div>
-      <GalleryItem />
-    </div>
-  );
-}
+const Gallery = () => {
+  const data = useContext(DataContext);
+
+  const display = data.map((item, index) => {
+    return <GalleryItem key={index} item={item} />;
+  });
+
+  return <div>{display}</div>;
+};
 
 export default Gallery;
